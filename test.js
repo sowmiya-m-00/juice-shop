de0ad6b7d6712b00aa1571b58f8c0393c683998c
sowmiya-m-00 <sowmiya.m@snyk.io>
2026-02-12 11:58:55 +0530

    Refactor test.js to enhance security
    Removed sensitive information and potential vulnerabilities from the test.js file.

--- test.js
+++ test.js
@@ -1,27 +1,12 @@
+const AWS_SECRET_KEY = "AKIAIMNO78987EXAMPLE"; 
+const database_password = "admin_password_123";
+
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
 const app = express();
-const db = new sqlite3.Database(':memory:');
 
-// 1. SQL Injection Vulnerability
-// Snyk should flag this because user input is concatenated directly into the query.
-app.get('/user', (req, res) => {
-    const query = "SELECT * FROM users WHERE id = " + req.query.id;
-    db.get(query, [], (err, row) => {
-        if (err) res.status(500).send(err);
-        res.send(row);
-    });
+// A very dangerous eval() call
+app.get('/execute', (req, res) => {
+    eval(req.query.code); // Remote Code Execution (RCE)
 });
 
-// 2. Path Traversal (Insecure Local File Inclusion)
-// Snyk should flag this because it allows users to potentially read any file on the system.
-app.get('/read-file', (req, res) => {
-    const filePath = './public/' + req.query.fileName;
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) res.status(404).send('File not found');
-        res.send(data);
-    });
-});
-
-app.listen(3000, () => console.log('Vulnerable server running on port 3000'));
+app.listen(3000);
